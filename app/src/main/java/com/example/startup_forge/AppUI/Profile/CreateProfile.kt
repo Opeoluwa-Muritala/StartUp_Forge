396c6ea1947cda3a0eb355de58c49491c996f388
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2024-02-19 10:06:43 +0100

    Calender.kt screen, CreateProfile.kt Screen

--- app/src/main/java/com/example/startup_forge/AppUI/Profile/CreateProfile.kt
+++ app/src/main/java/com/example/startup_forge/AppUI/Profile/CreateProfile.kt
@@ -2,21 +2,29 @@ package com.example.wasteup.ui.theme.Profile
 
 import android.text.BoringLayout
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.ScrollableState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -106,6 +114,9 @@ fun CreateProfile(
                 fontSize = 10.sp,
                 color = ThemeOrange
             )
+        }
+            //Select Expertise
+        Column {
             Text(
                 text = "Select Expertise",
                 fontWeight = Bold,
@@ -113,6 +124,43 @@ fun CreateProfile(
                 color = TextBlack
             )
             SelectionBox(label = "E.g: Branding, Marketing")
+            Button(
+                onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(20.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent
+                ),
+                modifier = Modifier.border(
+                    2.dp,
+                    TextBlack,
+                    RoundedCornerShape(20.dp)
+                )
+            ) {
+                Icon(imageVector = Icons.Outlined.Add, contentDescription = "Add Other Expertise")
+                Text("Add Other Expertise", color = TextBlack, fontSize = 15.sp)
+            }
+        }
+
+        //Skills
+        Column {
+            Text(
+                text = "Which skills do you have experience in?" ,
+                fontWeight = Bold,
+                fontSize = 15.sp,
+                color = TextBlack
+            )
+            SelectionBox(label = "Eg: Leadership, Communication")
+        }
+
+        //Bio
+        Column {
+            Text(
+                text = "Bio" ,
+                fontWeight = Bold,
+                fontSize = 15.sp,
+                color = TextBlack
+            )
+            TextField(value = "", onValueChange = {}, modifier = Modifier.heightIn(min= 600.dp))
         }
 
 
